14dde7b0c6fd6d3f0d032f1248627a5335a5dced
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2024-09-02 20:25:55 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define int long long
+
+using namespace std;
+int a, b;
+signed main(){
+  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  cin >> a >> b;
+  cout << a + b;
+  return 0;
+}
